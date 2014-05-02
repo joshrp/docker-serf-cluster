db68042ec61c453daa46459efebce4ab78e074e0
Josh Priestley <josh.priestley@bbc.co.uk>
2014-05-02 15:34:34 +0100

    Adding install script

--- setupHost.sh
+++ setupHost.sh
@@ -5,3 +5,5 @@ set -e
 sudo ln -s ~/docker-serf-cluster/host-serf-conf /etc/serf
 sudo ln -s /etc/serf/serf.init.sh /etc/init.d/serf
 sudo ln -s /etc/serf/serf-0.5.0 /usr/bin/serf
+
+./installDocker.sh
